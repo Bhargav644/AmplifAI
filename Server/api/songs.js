2b611a6554e2b715a831b43ea5faa5ca933307ba
chetan9129 <anku9129@gmail.com>
2023-06-07 19:30:42 +0530

    Backend Changes

--- Server/api/songs.js
+++ Server/api/songs.js
@@ -1,17 +1,4 @@
 const Songs = require("../models/songs")["model"];
-<<<<<<< HEAD
-class SongsAPI{
-    async getSongsByArtist(keyword){
-        try{
-            const artist_songs = await Songs.find({
-                artist_name: { $regex: keyword, $options: "i" },
-            });
-            return artist_songs;
-        }
-        catch(err){
-            throw new Error(err.message);
-        }
-=======
 class SongsAPI {
   async getSongsByArtist(keyword) {
     try {
@@ -24,17 +11,6 @@ class SongsAPI {
     }
   }
 
-  async getSongsByName(keyword) {
-    try {
-      const songs = await Songs.find({
-        song_name: { $regex: keyword, $options: "i" },
-      });
-      return songs;
-    } catch (err) {
-      throw new Error(err.message);
->>>>>>> 2eba741c11143f4dd5041979dd562a7b870831de
-    }
-  }
   async getSongsByAlbum(keyword) {
     try {
       const album_songs = await Songs.find({
@@ -44,22 +20,18 @@ class SongsAPI {
     } catch (err) {
       throw new Error(err.message);
     }
-<<<<<<< HEAD
-
-    async getSongsByTag(keyword){
-        try{
-            const tag_songs= await Songs.find({
-                tag: { $regex: songKeyword, $options: "i" },
-              });
-            return tag_songs;
-        }
-        catch(err){
-            throw new Error(err.message);
-        }
-    }
-=======
   }
->>>>>>> 2eba741c11143f4dd5041979dd562a7b870831de
+
+  async getSongsByTag(keyword) {
+    try {
+      const tag_songs = await Songs.find({
+        tag: { $regex: songKeyword, $options: "i" },
+      });
+      return tag_songs;
+    } catch (err) {
+      throw new Error(err.message);
+    }
+  }
 }
 
 module.exports = new SongsAPI();
