032d75575096b6dbd22ae2e9d95d09c5935e4c37
Bhargav Sharma <bhargav@pop-os.localdomain>
2023-06-09 03:27:18 +0530

    Detector Updated

--- Client/src/components/Detector/Detector.jsx
+++ Client/src/components/Detector/Detector.jsx
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import "./Detector.css";
 import axios from "axios";
 import PlaylistSection from "./PlaylistSection";
-import secureLocalStorage from "react-secure-storage";
+import  secureLocalStorage  from  "react-secure-storage";
 function Detector() {
   const [modelsLoaded, setModelsLoaded] = React.useState(false);
   const [emotions, setEmotions] = useState({
@@ -52,22 +52,16 @@ function Detector() {
 
   const callForAPI = (emotions) => {
     // console.log(emotions)
-    axios
-      .post("/getEmotionPlaylist", { emotion: emotions.emotion_type })
-      .then((res) => {
-        setEmotionPlaylists(res.data);
-        const playlistInStorage = JSON.parse(
-          secureLocalStorage.getItem("playlists")
-        );
-        const list = Object.values(playlistInStorage);
-        list.push(...Object.values(res.data));
-        const updatedPlaylist = Object.assign({}, list);
-        secureLocalStorage.setItem(
-          "playlists",
-          JSON.stringify(updatedPlaylist)
-        );
-      });
-  };
+    axios.post('/getEmotionPlaylist',{"emotion":emotions.emotion_type}).then((res)=>{
+      setEmotionPlaylists(res.data);
+      const playlistInStorage=JSON.parse(secureLocalStorage.getItem('playlists'));
+      const list=Object.values(playlistInStorage);
+      list.push(...Object.values(res.data));
+      const updatedPlaylist=Object.assign({},list);
+      secureLocalStorage.setItem('playlists',JSON.stringify(updatedPlaylist));
+    });
+
+  }
   React.useEffect(() => {
     if (isHandleVideoEnded && emotions.emotion_type !== "") {
       callForAPI(emotions);
@@ -99,21 +93,23 @@ function Detector() {
           detections,
           displaySize
         );
-        console.log(detections[0].expressions);
+        console.log(detections[0].expressions)
         const expressions = detections[0].expressions;
         const maxExpression = Math.max(...Object.values(expressions));
         const dominantEmotion = Object.keys(expressions).find(
           (key) => expressions[key] === maxExpression
         );
-
-        if (dominantEmotion === "surprised") {
-          setEmotions((prev) => ({ ...prev, emotion_type: "surprise" }));
-        } else if (dominantEmotion === "fearful") {
-          setEmotions((prev) => ({ ...prev, emotion_type: "fear" }));
-        } else {
-          setEmotions((prev) => ({ ...prev, emotion_type: dominantEmotion }));
+        
+        if(dominantEmotion==="surprised") {
+          setEmotions((prev)=>({...prev,emotion_type:"surprise"}));
         }
-        setEmotions((prev) => ({ ...prev, emotion_dominance: maxExpression }));
+        else if(dominantEmotion==="fearful"){
+          setEmotions((prev)=>({...prev,emotion_type:"fear"}));
+        }
+        else{
+          setEmotions((prev)=>({...prev,emotion_type: dominantEmotion}));
+        }
+        setEmotions((prev)=>({...prev,emotion_dominance: maxExpression}));
 
         canvasRef &&
           canvasRef.current &&
@@ -142,6 +138,7 @@ function Detector() {
       closeWebcam();
       setIsHandleVideoEnded(true);
     }, 4000);
+
   };
 
   const closeWebcam = () => {
